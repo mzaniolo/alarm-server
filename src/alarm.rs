b60296c2d281a0194ba5d02f0c70b61247c772b1
mzaniolo <mgzaniolo@gmail.com>
2024-03-19 01:39:55 +0000

    refactor to have an alarm mod

---
+++ src/alarm.rs
@@ -0,0 +1,61 @@
+use tokio::sync::broadcast;
+
+#[derive(Debug)]
+pub enum AlarmSevetity {
+    High,
+    Medium,
+    Low,
+}
+
+#[derive(Debug)]
+pub struct Alarm {
+    path: String,
+    set: i64,
+    reset: i64,
+    severity: AlarmSevetity,
+    meas: String,
+    rx: Option<broadcast::Receiver<i64>>,
+}
+
+impl Alarm {
+    pub fn new(
+        path: String,
+        set: i64,
+        reset: i64,
+        severity: AlarmSevetity,
+        meas: String,
+        rx: Option<broadcast::Receiver<i64>>,
+    ) -> Alarm {
+        Alarm {
+            path: path,
+            set: set,
+            reset: reset,
+            severity: severity,
+            meas: meas,
+            rx: rx,
+        }
+    }
+
+    pub async fn run(&mut self) {
+        println!("init run");
+        let rx = self.rx.as_mut().unwrap();
+        println!("waiting on channel!");
+        while let Ok(value) = rx.recv().await {
+            if value == self.set {
+                println!(
+                    "alarm {} is set with severity '{:?}' - value: {value}!",
+                    self.path, self.severity
+                );
+            } else if value == self.reset {
+                println!(
+                    "alarm {} is reset with severity '{:?}' - value: {value}!",
+                    self.path, self.severity
+                );
+            }
+        }
+    }
+
+    pub fn subscribe(&mut self, rx: broadcast::Receiver<i64>) {
+        self.rx = Some(rx);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,37 @@
 extern crate yaml_rust;
 use std::fs;
-use tokio::sync::broadcast;
 use yaml_rust::{Yaml, YamlLoader};
+
+mod alarm;
 
 pub fn load_config(path: &str) -> Yaml {
     let source = fs::read_to_string(path).unwrap();
     YamlLoader::load_from_str(&source).unwrap().remove(0)
 }
 
-#[derive(Debug)]
-pub enum AlarmSevetity {
-    High,
-    Medium,
-    Low,
-}
-
-#[derive(Debug)]
-pub struct Alarm {
-    path: String,
-    set: i64,
-    reset: i64,
-    sevetity: AlarmSevetity,
-    meas: String,
-    rx: Option<broadcast::Receiver<i64>>,
-}
-
-impl Alarm {
-    pub async fn run(&mut self) {
-        println!("init run");
-        let rx = self.rx.as_mut().unwrap();
-        println!("waiting on channel!");
-        while let Ok(value) = rx.recv().await {
-            if value == self.set {
-                println!(
-                    "alarm {} is set with severity '{:?}' - value: {value}!",
-                    self.path, self.sevetity
-                );
-            } else if value == self.reset {
-                println!(
-                    "alarm {} is reset with severity '{:?}' - value: {value}!",
-                    self.path, self.sevetity
-                );
-            }
-        }
-    }
-
-    pub fn subscribe(&mut self, rx: broadcast::Receiver<i64>) {
-        self.rx = Some(rx);
-    }
-}
-
-pub fn create_alarms(config: Yaml) -> Vec<Alarm> {
-    let mut alarmes: Vec<Alarm> = Vec::new();
+pub fn create_alarms(config: Yaml) -> Vec<alarm::Alarm> {
+    let mut alarmes: Vec<alarm::Alarm> = Vec::new();
 
     if let Yaml::Hash(ref h) = config {
         for (area, alm) in h {
             if let Yaml::Hash(h) = alm {
-                for (alm, values) in h {
-                    alarmes.push(Alarm {
-                        path: format!("{}/{}", area.as_str().unwrap(), area.as_str().unwrap()),
-                        set: values["set"].as_i64().expect("Alarm need field 'set'"),
-                        reset: values["reset"].as_i64().expect("Alarm need field 'reset'"),
-                        meas: String::from(
-                            values["meas"].as_str().expect("Alarm need field 'meas'"),
-                        ),
-                        sevetity: match values["sevetity"]
+                for (_, values) in h {
+                    alarmes.push(alarm::Alarm::new(
+                        format!("{}/{}", area.as_str().unwrap(), area.as_str().unwrap()),
+                        values["set"].as_i64().expect("Alarm need field 'set'"),
+                        values["reset"].as_i64().expect("Alarm need field 'reset'"),
+                        match values["sevetity"]
                             .as_i64()
                             .expect("Alarm need field 'sevetity'")
                         {
-                            2 => AlarmSevetity::High,
-                            1 => AlarmSevetity::Medium,
-                            0 => AlarmSevetity::Low,
+                            2 => alarm::AlarmSevetity::High,
+                            1 => alarm::AlarmSevetity::Medium,
+                            0 => alarm::AlarmSevetity::Low,
                             _ => panic!("Invalid sevetity!"),
                         },
-                        rx: None,
-                    })
+                        String::from(values["meas"].as_str().expect("Alarm need field 'meas'")),
+                        None,
+                    ))
                 }
             }
         }
