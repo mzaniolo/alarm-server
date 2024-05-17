1066183b230a15edb2115082cc8a714f95e1d04d
Matheus Zaniolo <mgzaniolo@gmail.com>
2024-05-17 13:21:43 +0000

    Added unit tests

--- alarm-server/src/config.rs
+++ alarm-server/src/config.rs
@@ -3,12 +3,14 @@ use std::fs;
 use toml;
 
 pub fn read_config(path: &str) -> Config {
-    let source = fs::read_to_string(path).expect("config file not found");
+    let source =
+        fs::read_to_string(path).expect(&format!("config file not found. Path: '{}'", path));
 
     toml::from_str(&source).expect("Invalid configuration file")
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default)]
+#[serde(default)]
 pub struct Config {
     pub broker: BrokerConfig,
     pub server: ServerConfig,
@@ -45,6 +47,34 @@ pub struct AlarmConfig {
     pub path: String,
 }
 
+impl Default for AlarmConfig {
+    fn default() -> Self {
+        Self {
+            path: default_path(),
+        }
+    }
+}
+
+impl Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            ip: default_ip(),
+            port: default_port::<8080>(),
+        }
+    }
+}
+
+impl Default for BrokerConfig {
+    fn default() -> Self {
+        Self {
+            ip: default_ip(),
+            port: default_port::<5672>(),
+            username: default_cred(),
+            password: default_cred(),
+        }
+    }
+}
+
 fn default_ip() -> String {
     "127.0.0.1".to_string()
 }
@@ -59,4 +89,70 @@ fn default_path() -> String {
 
 const fn default_port<const T: u16>() -> u16 {
     T
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_example() -> Result<(), Box<dyn std::error::Error>> {
+        let config = read_config("examples/server_config.toml");
+
+        assert_eq!(config.alarm.path, "examples/config.yaml");
+
+        assert_eq!(config.server.ip, "127.0.0.1");
+        assert_eq!(config.server.port, 8080);
+
+        assert_eq!(config.broker.ip, "127.0.0.1");
+        assert_eq!(config.broker.port, 5672);
+        assert_eq!(config.broker.username, "guest");
+        assert_eq!(config.broker.password, "guest");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_empty() -> Result<(), Box<dyn std::error::Error>> {
+        let config: Config = toml::from_str("").expect("Invalid configuration file");
+
+        assert_eq!(config.alarm.path, "examples/config.yaml");
+
+        assert_eq!(config.server.ip, "127.0.0.1");
+        assert_eq!(config.server.port, 8080);
+
+        assert_eq!(config.broker.ip, "127.0.0.1");
+        assert_eq!(config.broker.port, 5672);
+        assert_eq!(config.broker.username, "guest");
+        assert_eq!(config.broker.password, "guest");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_missing_fields() -> Result<(), Box<dyn std::error::Error>> {
+        let config = r#"
+            [broker]
+            port = 5672
+            password = "guest"
+
+            [server]
+            port = 8080
+        "#;
+
+        let config: Config = toml::from_str(config).expect("Invalid configuration file");
+
+        assert_eq!(config.alarm.path, "examples/config.yaml");
+
+        assert_eq!(config.server.ip, "127.0.0.1");
+        assert_eq!(config.server.port, 8080);
+
+        assert_eq!(config.broker.ip, "127.0.0.1");
+        assert_eq!(config.broker.port, 5672);
+        assert_eq!(config.broker.username, "guest");
+        assert_eq!(config.broker.password, "guest");
+
+        Ok(())
+    }
 }
